b20052cdbd288716ba7fbe1e85b76342da84c984
BurakGozen <info@newcode.be>
2024-03-07 15:08:08 +0100

    feat: deployment ready

--- 1-seo/app/robots.ts
+++ 1-seo/app/robots.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from "next";
+import { BASE_URL } from "../lib/const";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -6,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "http://localhost:3000/sitemap.xml",
+    sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
--- 1-seo/app/sitemap.ts
+++ 1-seo/app/sitemap.ts
@@ -1,9 +1,10 @@
 import { MetadataRoute } from "next";
+import { BASE_URL } from "../lib/const";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "http://localhost:3000",
+      url: BASE_URL,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
