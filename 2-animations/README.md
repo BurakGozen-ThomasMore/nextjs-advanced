5c5649e144a3a295792a411a6e09b59bee16d938
BurakGozen <info@newcode.be>
2024-03-12 14:28:59 +0100

    feat: variants exercise

--- 2-animations/README.md
+++ 2-animations/README.md
@@ -5,6 +5,9 @@
 1. De bal dient continu te stuiteren in een eindeloze cyclus
 2. De tekst moet van onderen naar boven bewegen, vanachter het blauwe vlak naar het midden van het scherm
 3. De lijn in de SVG moet geleidelijk verschijnen, beginnend vanaf nul tot het volledig gevormd is
+4. De chevron geeft springend aan dat je naar beneden moet scrollen.
+5. De ul moet geleidelijk van grootte veranderen, van niets naar zijn volledige grootte.
+6. De li items moeten na elkaar verschijnen, waarbij elk item één voor één wordt weergegeven in plaats van allemaal tegelijk.
 
 ## Documentatie
 
--- 2-animations/src/app/variants/page.tsx
+++ 2-animations/src/app/variants/page.tsx
@@ -5,43 +5,19 @@ import { ChevronDown } from "lucide-react";
 
 const MotionChevronDown = motion(ChevronDown);
 
-export default function BallAnimation() {
-  const container = {
-    hidden: { scale: 0 },
-    visible: { scale: 1 },
-  };
-
-  const item = {
-    hidden: { y: -50, opacity: 0 },
-    visible: { y: 0, opacity: 1 },
-  };
-
+export default function VariantsAnimation() {
   return (
     <main className="bg-slate-800 font-mono flex flex-col">
       <div className="h-screen flex flex-col justify-center items-center gap-5">
         <h1 className="text-5xl text-white">Scroll naar beneden</h1>
-        <MotionChevronDown
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 1.5, repeat: Infinity, repeatType: "reverse" }}
-          className="w-12 h-12 text-white"
-        />
+        <MotionChevronDown className="w-12 h-12 text-white" />
       </div>
 
-      <motion.ul
-        className="flex flex-col items-center bg-white/40 p-10 rounded-3xl m-24"
-        initial="hidden"
-        whileInView="visible"
-        variants={container}
-        transition={{ staggerChildren: 0.1, delayChildren: 0.5 }}
-      >
+      <ul className="flex flex-col items-center bg-white/40 p-10 rounded-3xl m-24">
         {Array.from({ length: 10 }).map((_, index) => (
-          <motion.li
-            key={index}
-            variants={item}
-            className="bg-white/50 w-full h-20 mb-4 rounded-xl"
-          />
+          <li key={index} className="bg-white/50 w-full h-20 mb-4 rounded-xl" />
         ))}
-      </motion.ul>
+      </ul>
     </main>
   );
 }
